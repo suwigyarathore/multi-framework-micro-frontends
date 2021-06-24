09cc034c14727bc166b9c7422dade0e1498865f3
Suwigya Rathore <suwigya@kramphub.com>
2021-06-24 20:16:39 +0200

    fixing history sync

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,10 +1,20 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useRef} from 'react';
+import { useHistory } from 'react-router-dom';
 import {mount} from 'marketing/MarketingApp'
 
 function MarketingApp() {
     const ref = useRef(null);
+    const history = useHistory();
+
     useEffect(() => {
-        mount(ref.current)
+     const { onParentNavigate } = mount(ref.current, {
+            onNavigate: ({ pathname: nextPathName }) => {
+                const { pathname } =  history.location;
+                pathname !== nextPathName && history.push(nextPathName);
+            }
+        });
+     history.listen(onParentNavigate);
+
     }, [])
     return <div ref={ref}/>;
 }
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 import App from './App';
 
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+    onNavigate && history.listen(onNavigate)
     ReactDOM.render(
-        <App/>,
+        <App history={history}/>,
         el
     )
+    return {
+        onParentNavigate({pathname: nextPathName}) {
+            const { pathname } =  history.location;
+            pathname !== nextPathName && history.push(nextPathName);
+        }
+    }
 }
 
 if(process.env.NODE_ENV === 'development') {
     const el = document.querySelector('#_marketing-dev-root');
-    el && mount(el);
+    el && mount(el, { defaultHistory: createBrowserHistory()});
 }
 
 
